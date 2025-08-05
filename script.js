7f6bc16938c1c76051c83c20a04892525450c758
Arnab Das <darnab628@gmail.com>
2025-08-06 01:10:40 +0530

    made a fuction to select grid numbers and made their h and w dynamic instead of fixed in css

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const container = document.querySelector(".container")
 
-for (let index = 0; index < 16*16; index++) {
+const grid=(numberOfGrids)=>{
+    container.innerHTML=""
+    const size = 512
+    for (let index = 0; index < numberOfGrids*numberOfGrids; index++) {
     const square = document.createElement("div")
     square.classList.add("square")
+    const height = size / numberOfGrids
+    const width = size / numberOfGrids
+    square.style.height=`${height}px`
+    square.style.width=`${width}px`
     container.appendChild(square)    
-}
-
-const squares = document.querySelectorAll(".square")
-squares.forEach((square)=>{
-    square.addEventListener("mouseenter" ,()=>{
-        square.style.backgroundColor="darkblue"
+    const squares = document.querySelectorAll(".square")
+    squares.forEach((square)=>{
+        square.addEventListener("mouseenter" ,()=>{
+            square.style.backgroundColor="darkblue"
+        })
     })
-})
+}}
+
+grid(16)
+
+
 
 const resetButton = document.querySelector(".reset")
 resetButton.addEventListener("click",()=>{
